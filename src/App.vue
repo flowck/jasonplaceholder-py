dfd65eeb9270bffc4323cc475303404943eab450
Firmino Changani <flowck96@gmail.com>
2018-01-26 14:05:20 +0100

    Install vue-resource

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+	<div id="app">
+		<header class="main-header container">
+			<h1>JSONPLACEHOLDER</h1>
+		</header>
+		<div class="container">
+			<router-view/>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+	export default {
+		name: 'App'
+	}
 </script>
 
 <style>
-
-  /* Global CSS */
-  @import '../node_modules/bulma/css/bulma.css'
-
-  .wrap{
-    width: 800px;
-    margin: 0 auto;
-  }
-
-
+	/* Global CSS */
+	@import '../node_modules/bulma/css/bulma.css';
+	
+	.container{
+		width: 800px;
+		margin: 0 auto;
+		border: 1px solid red;
+	}
 </style>
