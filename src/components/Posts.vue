5812cbd6280f579acbd1ee21f233e8065a1e6bc6
Firmino Changani <flowck96@gmail.com>
2018-01-26 14:20:33 +0100

    Implement get post method

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,12 +1,46 @@
 <template>
 	<div>
 		<h1>Posts</h1>
+		<hr>
+		<article class="card" v-for="post in posts">
+			<div class="card-content">
+				<h1 class="title is-4">{{ post.title }}</h1>
+				<p> {{ post.body }} </p>
+			</div>
+		</article>
 	</div>
 </template>
 
 <script>
 	export default {
 		name: 'Posts',
-		data: {}
+		data: () => {
+			return {
+				posts: []
+			}
+		},
+		created() {
+			this.getPosts();
+		},
+		methods: {
+			getPosts() {
+				this.$http.get('https://jsonplaceholder.typicode.com/posts')
+				.then(
+					(success) => {
+						this.posts = success.body;
+					},
+					(error) => {
+						console.log(error);
+					}
+				)
+			}
+		},
 	}
 </script>
+
+<style>
+	.title{
+		text-transform: uppercase;
+	}
+</style>
+
