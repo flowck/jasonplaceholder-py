070d59ed28d3fe5346be42f67b0275f88ce915f3
Firmino Changani <flowck96@gmail.com>
2018-01-27 19:38:15 +0100

    Add post custom url and styles

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
 	<div id="app">
+		<!-- Header start here -->
 		<header class="main-header container">
-			<h1>JSONPLACEHOLDER</h1>
+			<div class="page-title">
+				<h1>
+					<a href="/">Jasonplaceholder</a>
+				</h1>
+			</div>
 		</header>
+		<!-- Header ends here -->
 		<div class="container">
 			<router-view/>
 		</div>
@@ -19,9 +25,23 @@
 	/* Global CSS */
 	@import '../node_modules/bulma/css/bulma.css';
 	
+	header .page-title{
+		font-size: 40px;
+		text-align: center;
+		font-family: 'Satisfy', cursive;
+	}
+
+	header .page-title a{
+		text-decoration: none;
+	}
+
+	.space{
+		width: 100%;
+		height: 40px;
+	}
+
 	.container{
 		width: 800px;
 		margin: 0 auto;
-		border: 1px solid red;
 	}
 </style>
